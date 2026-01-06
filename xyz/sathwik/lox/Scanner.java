90c27004bc92671f2836becc0e9c019b8942743c
sathwikv2005 <sathwikv2005@gmail.com>
2026-01-06 14:35:29 +0530

    handle strings

--- xyz/sathwik/lox/Scanner.java
+++ xyz/sathwik/lox/Scanner.java
@@ -99,6 +99,13 @@ class Scanner {
             case '\t':
                 // ignore whitespaces
                 break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                string();
+                break;
+
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
@@ -132,6 +139,21 @@ class Scanner {
         if (isAtEnd())
             return '\0';
         return source.charAt(current);
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+
+        if (isAtEnd())
+            Lox.error(line, "Unterminated string.");
+
+        advance();
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
 }
