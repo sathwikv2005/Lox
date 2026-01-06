72bfa4ff36c546633092d033eb1d9389cb0d86f6
sathwikv2005 <sathwikv2005@gmail.com>
2026-01-07 03:07:35 +0530

    handle keywords

--- xyz/sathwik/lox/Scanner.java
+++ xyz/sathwik/lox/Scanner.java
@@ -14,6 +14,29 @@ class Scanner {
     private int current = 0;
     private int line = 1;
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+
+    }
+
     Scanner(String source) {
         this.source = source;
     }
@@ -26,10 +49,6 @@ class Scanner {
 
         tokens.add(new Token(EOF, "", null, line));
         return tokens;
-    }
-
-    private boolean isAtEnd() {
-        return current >= source.length();
     }
 
     private void scanToken() {
@@ -109,33 +128,21 @@ class Scanner {
             default:
                 if (isDigit(c))
                     number();
+                else if (isAlpha(c))
+                    identifier();
                 else
                     Lox.error(line, "Unexpected character.");
                 break;
         }
     }
 
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
     private char advance() {
         current++;
         return source.charAt(current - 1);
-    }
-
-    private void addToken(TokenType type) {
-        addToken(type, null);
-    }
-
-    private void addToken(TokenType type, Object literal) {
-        String text = source.substring(start, current);
-        tokens.add(new Token(type, text, literal, line));
-    }
-
-    private boolean match(char expected) {
-        if (isAtEnd())
-            return false;
-        if (source.charAt(current) != expected)
-            return false;
-        current++;
-        return true;
     }
 
     private char peek() {
@@ -148,6 +155,49 @@ class Scanner {
         if (current + 1 >= source.length())
             return '\0';
         return source.charAt(current + 1);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
+        current++;
+        return true;
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 
     private void string() {
@@ -165,21 +215,15 @@ class Scanner {
         addToken(STRING, value);
     }
 
-    private boolean isDigit(char c) {
-        return c >= '0' && c <= '9';
-    }
-
-    private void number() {
-        while (isDigit(peek()))
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
             advance();
 
-        if (peek() == '.' && isDigit(peekNext())) {
-            advance();
-            while (isDigit(peek()))
-                advance();
-        }
-
-        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+        addToken(type);
     }
 
 }
