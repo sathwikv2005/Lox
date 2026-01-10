53d750e91257229c565f4924fef110e275018516
sathwikv2005 <sathwikv2005@gmail.com>
2026-01-11 01:34:43 +0530

    add expression class

---
+++ xyz/sathwik/tool/GenerateAst.java
@@ -0,0 +1,94 @@
+package xyz.sathwik.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: java GenerateAst <output dir>");
+            System.exit(64);
+        }
+
+        String outPutDir = args[0];
+
+        defineAst(outPutDir, "Expr", Arrays.asList(
+                "Binary : Expr left, Token operator, Expr right",
+                "Grouping   : Expr expression",
+                "Literal    : Object value",
+                "Unary      : Token operator, Expr right"));
+
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + '/' + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package xyz.sathwik.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        for (String type : types) {
+            writer.println();
+            String[] split_arr = type.split(":");
+            String className = split_arr[0].trim();
+            String fields = split_arr[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println();
+
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
+        writer.println();
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fields) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        // constructor
+        writer.println("        " + className + " (" + fields + ") {");
+        String[] fieldsArr = fields.split(", ");
+
+        for (String field : fieldsArr) {
+            String name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+        writer.println("}");
+        writer.println();
+
+        // Visitor pattern
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("        return visitor.visit" + className + baseName + "(this);");
+        writer.println("}");
+
+        writer.println();
+        // fields
+        for (String field : fieldsArr) {
+            writer.println("        final " + field + ";");
+        }
+
+        writer.println();
+        writer.println("}");
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String name = type.split(":")[0].trim();
+            writer.println("        R visit" + name + baseName + "(" + name + " " + baseName.toLowerCase() + "); ");
+        }
+        writer.println("}");
+    }
+
+}
