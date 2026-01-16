61bdb902c0b8dd9a7e81dbd63e53c12824cd0804
sathwikv2005 <sathwikv2005@gmail.com>
2026-01-17 02:36:32 +0530

    add for loops

--- xyz/sathwik/lox/Parser.java
+++ xyz/sathwik/lox/Parser.java
@@ -2,6 +2,7 @@ package xyz.sathwik.lox;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 import static xyz.sathwik.lox.TokenType.*;
 
 public class Parser {
@@ -228,6 +229,8 @@ public class Parser {
     }
 
     private Stmt statement() {
+        if (match(FOR))
+            return forStatement();
         if (match(IF))
             return ifStatement();
         if (match(PRINT))
@@ -237,6 +240,45 @@ public class Parser {
         if (match(LEFT_BRACE))
             return new Stmt.Block(block());
         return expressionStatement();
+    }
+
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after for.");
+
+        Stmt initializer;
+        if (match(SEMICOLON))
+            initializer = null;
+        else if (match(VAR))
+            initializer = varDeclaration();
+        else
+            initializer = expressionStatement();
+
+        Expr condition = null;
+        if (!check(SEMICOLON))
+            condition = expression();
+        consume(SEMICOLON, "Expect ';' after loop condition.");
+
+        Expr increment = null;
+        if (!check(RIGHT_PAREN))
+            increment = expression();
+
+        consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+        }
+        if (condition == null)
+            condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
+
     }
 
     private Stmt ifStatement() {
@@ -259,7 +301,7 @@ public class Parser {
     }
 
     private Stmt whileStatement() {
-        consume(LEFT_PAREN, "Expect ( after 'while'.");
+        consume(LEFT_PAREN, "Expect '(' after while.");
         Expr condition = expression();
         consume(RIGHT_PAREN, "Expect ')' after while condition.");
         Stmt body = statement();
