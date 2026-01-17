8ac2038626146ddba5d88693bc374d657ce2d3bb
sathwikv2005 <sathwikv2005@gmail.com>
2026-01-17 22:52:48 +0530

    add: function closures

--- xyz/sathwik/lox/LoxFunction.java
+++ xyz/sathwik/lox/LoxFunction.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment environment) {
         this.declaration = declaration;
+        this.closure = environment;
     }
 
     public Object call(Interpreter interpreter, List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
         }
